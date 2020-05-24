1b74ea82a69a62699b9ab7a69ca539e6743403e5
Avinash <Avinash@DESKTOP-4TJH50L>
2020-05-24 17:41:35 +0530

    Commit test2

---
+++ Test2/src/com/avinash/HelloTest.java
@@ -0,0 +1,11 @@
+package com.avinash;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git!! This is another test project.");
+
+	}
+
+}
