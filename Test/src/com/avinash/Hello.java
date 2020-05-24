1f2250eae5379ffdd88055d51d6aaa677ef86c4f
avinash-rana-ofc <65813570+avinash-rana-ofc@users.noreply.github.com>
2020-05-24 18:53:32 +0530

    Update Hello.java

--- Test/src/com/avinash/Hello.java
+++ Test/src/com/avinash/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello! Tetsing Git");
 		System.out.println("Comparing");
+		
+		System.out.println("Pulling");
 	}
 
 }
